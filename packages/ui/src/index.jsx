3d8f74feb8bd1b2f766b14c38a0f7c10de15e82d
amin.chiha@gmail.com <amin.chiha@gmail.com>
2024-08-07 05:57:46 +0100

    added Prisma

---
+++ packages/ui/src/index.jsx
@@ -0,0 +1,14 @@
+export { Card } from "./card";
+export { useToast } from "./Shadcn/use-toast";
+export { Toaster } from "./Shadcn/toaster";
+export {
+  ToastProvider,
+  ToastViewport,
+  Toast,
+  ToastTitle,
+  ToastDescription,
+  ToastClose,
+  ToastAction,
+} from "./Shadcn/toast";
+
+export { Button, buttonVariants } from "./Shadcn/button";
